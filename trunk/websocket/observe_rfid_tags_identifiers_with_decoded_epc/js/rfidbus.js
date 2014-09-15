b96ff04edef4bdb4c1e1e80850038dee9124b186
info@rfidcenter.ru <info@rfidcenter.ru@89ac5766-e617-bd0f-5619-8ccf7c6aaf70>
2014-09-15 16:11:22 +0000

    Добавлен «Observe RFID tags identifiers with decoded EPC»: Пример подключения идентификаторов меток со всех доступных Шине RFID считывателей с расшифровкой EPC.
    {"ReaderRecord":{"Id":"16fba3f9-d8a6-4f69-8c91-86945753669c","Name":"Impinj R420 - .165","Description":"","ReaderType":0,"Device":{"Id":"e4cbad53-b4c3-4ca3-b3f8-87642b9f4788","Name":"LLRP device","Description":"Library to work with LLRP devices."},"IsOpen":true,"Mode":1,"IsActive":true,"DeactivatedBy":0,"DeactivationReason":null},"Transponders":[{"Type":19,"Id":"MDld+oDbToAACUQ7","Antenna":1,"Data":null,"BlockSize":2,
            "Extended":{
                     "Groups":[],
                     "Items":[
                             {"Name":"EpcFilter", "Value":"1"}, 
                             {"Name":"EpcGln","Value":"358378"},
                             {"Name":"EpcItem","Value":"224570"},
                             {"Name":"EpcSerial","Value":"607291"}],
                      "IsEmpty":false
             },
            "IdAsString":"30395DFA80DB4E800009443B"}],"Name":"TransponderFoundEvent"}

--- trunk/websocket/observe_rfid_tags_identifiers_with_decoded_epc/js/rfidbus.js
+++ trunk/websocket/observe_rfid_tags_identifiers_with_decoded_epc/js/rfidbus.js
@@ -1,13 +1,16 @@
 var responseHandler = new RfidBusResponseHandler();
 
 responseHandler.AddHandler("AuthorizeResponse", function() {
+
 	requestSender.SendGetReaders();
 });
 
 responseHandler.AddHandler("GetReadersResponse", function(msg) {
 	msg.Readers.forEach(function (eachName) {
+		requestSender.SendEnableDecodeEpc(eachName.Id);
 		requestSender.SendSubscribeToReader(eachName.Id);
 		requestSender.SendStartReading(eachName.Id);
+
 	});
 });
 
@@ -15,7 +18,7 @@ var webSocketClient = new WebSocketClient(responseHandler);
 var requestSender = new RfidBusRequestSender(webSocketClient, responseHandler);
 
 webSocketClient.AddEventHandler(WebSocketClient.EVENT_ON_OPEN, function() {
-	requestSender.SendAuthorize("demo", "demo")
+	requestSender.SendAuthorize("demo", "demo");
 });
 
 webSocketClient.Open("ws://demo.rfidbus.rfidcenter.ru:80");
