01153aa7db7eacef2d846586d564a24cd5f1d54a
info@rfidcenter.ru <info@rfidcenter.ru@89ac5766-e617-bd0f-5619-8ccf7c6aaf70>
2014-09-23 14:54:18 +0000

    * csharp examples

---
+++ trunk/csharp/ComAsClassExample/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+using RfidBus.Com;
+
+namespace ComAsClassExample
+{
+    internal class Program
+    {
+        private RfidBusComClient _client = null;
+
+        private static void Main(string[] args)
+        {
+            var program = new Program();
+
+            try
+            {
+                program.DoWork();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Caught exception {0}: '{1}'.", ex.GetType(), ex.Message);
+            }
+            finally
+            {
+                program.Finish();
+
+                WaitForKey("Press ENTER to exit.", ConsoleKey.Enter);
+            }
+        }
+
+        private void DoWork()
+        {
+            Console.WriteLine("Establishing connection to RFID Bus...");
+            this._client = new RfidBusComClient();
+            if(!this._client.Connect("127.0.0.1", 20000, "admin", "admin"))
+                throw new Exception("Can't establishe connection to RFID Bus.");
+            Console.WriteLine("Connection established.");
+
+            this._client.TransponderFound += this.ComClientOnTransponderFound;
+
+            Console.WriteLine("Subscribing to readers...");
+            var readers = this._client.GetReaders();
+            foreach(var reader in readers)
+            {
+                this._client.SubscribeToReader(reader.Id);
+                this._client.StartReading(reader.Id);
+            }
+
+            WaitForKey("Press ESC to stop.", ConsoleKey.Escape);
+        }
+
+        private void ComClientOnTransponderFound(string reader, string tid)
+        {
+            Console.WriteLine("> Reader '{0}' found transponder '{1}'.", reader, tid);
+        }
+
+        private void Finish()
+        {
+            if(this._client == null)
+                return;
+
+            this._client.Close();
+        }
+
+        private static void WaitForKey(string message, ConsoleKey key)
+        {
+            Console.WriteLine();
+            Console.WriteLine(message);
+            while(Console.ReadKey(true)
+                         .Key != key)
+            {
+            }
+        }
+    }
+}
---
+++ trunk/csharp/readme.txt
@@ -0,0 +1,14 @@
+﻿I. Предстваленные примеры
+
+* ComAsClassExample - пример использования COM/Active-X объекта для работы с Шиной RFID в качестве обычного класса .NET.
+* RfidBusClientExample - пример использования класса RfidBusClient для работы с Шиной RFID.
+
+II. Зависимости
+
+1. ComAsClassExample
+
+Проект ComAsClassExample зависит от сборок RfidBus.Com.Primitives.dll и RfidBus.Com.dll, которые можно найти по месту установки Шины RFID (обычно: «c:\Program Files (x86)\RfidCenter\RfidBus\»).
+
+2. RfidBusClientExample
+
+Проект RfidBusClientExample зависит от сборок RfidCenter.Basic.dll, RfidCenter.Devices.dll, RfidBus.Primitives.dll, расположенных по месту установки Шины RFID, и RfidBus.Serializers.Pb.dll, расположенной в поддиректории «serializers» в корневой директории Шины RFID.
